74f9e94e97b9a6f71823583f0d48719259b608cb
kwau <kwau@ualberta.ca>
2015-04-07 12:51:11 -0600

    add sql database file
    for when we make changes in the future to the database.

---
+++ iHuman_database.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+	id tinyint unsigned NOT NULL auto_increment PRIMARY KEY,
+	firstName varchar(30) NOT NULL,
+	lastName varchar(30) NOT NULL,
+	dateOfBirth DATE,
+	username varchar(30) NOT NULL,
+	password varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS clock;
+CREATE TABLE clock (
+	id tinyint unsigned NOT NULL auto_increment PRIMARY KEY,
+	username tinyint NOT NULL,
+	timeIn timestamp DEFAULT 0,
+	timeOut timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS eventCompletion;
+CREATE TABLE eventCompletion (
+	id tinyint unsigned NOT NULL auto_increment PRIMARY KEY,
+	username tinyint NOT NULL,
+	eventName tinyint NOT NULL,
+	dateComplete date NOT NULL,
+	journal text
+);
+
+DROP TABLE IF EXISTS event;
+CREATE TABLE event (
+	id tinyint unsigned NOT NULL auto_increment PRIMARY KEY,
+	eventName varchar(50) NOT NULL,
+	eventCategory varchar(30) NOT NULL,
+	eventLocation varchar(30) NOT NULL,
+	points int NOT NULL
+);
+
+DROP TABLE IF EXISTS mtmRegistry;
+CREATE TABLE mtmRegistry (
+	id tinyint unsigned NOT NULL auto_increment PRIMARY KEY,
+	username tinyint NOT NULL,
+	dateComplete date NOT NULL,
+	mood tinyint NOT NULL,
+	journal text
+);
