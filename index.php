5bb4b789cb62321955ed3c51e68874e575af5152
kwau <kwau@ualberta.ca>
2015-04-08 16:42:13 -0600

    changed order
    changed button order.

--- index.php
+++ index.php
@@ -39,51 +39,6 @@ Password: <input type='password' name='password' id='password' value='' maxlengt
 <input type='submit' name='loginSubmit' id='loginSubmit' value='Log In'>
 </form>
 
-<div id="displayPassword" name="displayPassword" style="display:none;" class="answer_list">
-<h2>Forgot Password</h2>
-
-<form name='forgotPassword' id='forgotPassword' method='post'>
-Username: <input type='text' name='r_username' id='r_username' value='' maxlength='30'><br><br>
-Date of Birth:
-	<select id='user_input_day' name='user_input_day'>
-		<?php
-			print "<option value='01' selected>1</option>";
-			for($i=2; $i<32; $i++){
-				if($i<10){
-					print "<option value='0$i'>$i</option>";
-				}else{
-					print "<option value='$i'>$i</option>";
-				};
-			};
-		?>
-	</select>
-	<select id='user_input_month' name='user_input_month'>
-		<option value='01' selected>January</option>
-		<option value='02' >February</option>
-		<option value='03' >March</option>
-		<option value='04' >April</option>
-		<option value='05' >May</option>
-		<option value='06' >June</option>
-		<option value='07' >July</option>
-		<option value='08' >August</option>
-		<option value='09' >September</option>
-		<option value='10' >October</option>
-		<option value='11' >November</option>
-		<option value='12' >December</option>
-	</select>
-	<select id='user_input_year' name='user_input_year'>
-		<?php
-			for($i=2015; $i>1900; $i--){
-				print "<option value='$i'>$i</option>";
-			};
-		?>
-	</select><br><br>
-New Password: <input type='password' name='newPassword' id='newPassword' value='' maxlength='30'><br><br>
-<input type='submit' name='forgotPasswordSubmit' id='forgotPasswordSubmit' value='Change Password'>
-</form>
-</div>
-<input type="button" id="forgotButton" name="forgotButton" value="Forgot Password" onclick="showPassword()"/>
-
 <div id="displayRegister" name="displayRegister" style="display:none;" class="answer_list">
 <form name='user_input' id='user_input' method='post'>
 	<h2>Register New User</h2>
@@ -136,6 +91,51 @@ New Password: <input type='password' name='newPassword' id='newPassword' value='
 </form>
 </div>
 <input type="button" id="registerButton" name="registerButton" value="Register Account" onclick="showRegister()"/>
+
+<div id="displayPassword" name="displayPassword" style="display:none;" class="answer_list">
+<h2>Forgot Password</h2>
+
+<form name='forgotPassword' id='forgotPassword' method='post'>
+Username: <input type='text' name='r_username' id='r_username' value='' maxlength='30'><br><br>
+Date of Birth:
+	<select id='user_input_day' name='user_input_day'>
+		<?php
+			print "<option value='01' selected>1</option>";
+			for($i=2; $i<32; $i++){
+				if($i<10){
+					print "<option value='0$i'>$i</option>";
+				}else{
+					print "<option value='$i'>$i</option>";
+				};
+			};
+		?>
+	</select>
+	<select id='user_input_month' name='user_input_month'>
+		<option value='01' selected>January</option>
+		<option value='02' >February</option>
+		<option value='03' >March</option>
+		<option value='04' >April</option>
+		<option value='05' >May</option>
+		<option value='06' >June</option>
+		<option value='07' >July</option>
+		<option value='08' >August</option>
+		<option value='09' >September</option>
+		<option value='10' >October</option>
+		<option value='11' >November</option>
+		<option value='12' >December</option>
+	</select>
+	<select id='user_input_year' name='user_input_year'>
+		<?php
+			for($i=2015; $i>1900; $i--){
+				print "<option value='$i'>$i</option>";
+			};
+		?>
+	</select><br><br>
+New Password: <input type='password' name='newPassword' id='newPassword' value='' maxlength='30'><br><br>
+<input type='submit' name='forgotPasswordSubmit' id='forgotPasswordSubmit' value='Change Password'>
+</form>
+</div>
+<input type="button" id="forgotButton" name="forgotButton" value="Forgot Password" onclick="showPassword()"/>
 
 </body>
 </html>
